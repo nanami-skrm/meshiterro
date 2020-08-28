4f9a518db7080c3ea66f6d5f53f1974fc7c171aa
@nanami-skrm <skrm773@gmail.com>
2020-08-28 22:28:27 +0900

    [Finish]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8daa1ea86aff12ba765f7ae050f046526fd82f1d28ba6a6c6d856ea55ec6da995546174f3de22718832544f1c4c352a336af072210b48684190dfeacf3f73b5a'
