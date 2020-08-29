b2245119a35b313206681a0ad98708db403c8e97
@nanami-skrm <skrm773@gmail.com>
2020-08-29 17:54:25 +0900

    [Finish]5-6

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy #dependent: :destroy(1が消えたらNのデータも全部消える)
   has_many :favorites, dependent: :destroy
+  attachment :profile_image #attachmentにはカラム名から_idを除いた名前を記述する
 end
